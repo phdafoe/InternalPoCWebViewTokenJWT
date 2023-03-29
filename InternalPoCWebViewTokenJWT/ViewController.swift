6d0a2b1a263c11835a08d288bfbe4555e76ffac8
Phdafoe <phdafoe@gmail.com>
2023-03-29 10:49:50 +0200

    Initial Commit

---
+++ InternalPoCWebViewTokenJWT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InternalPoCWebViewTokenJWT
+//
+//  Created by TECDATA ENGINEERING on 29/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
