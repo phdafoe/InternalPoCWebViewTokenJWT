742aa80f161a8f9c7188ee2556396f1ed612e93c
Phdafoe <phdafoe@gmail.com>
2023-05-23 09:11:44 +0200

    [WIP] - Set cookie jwtToken in array cookies when user invoque a url private zone

--- InternalPoCWebViewTokenJWT/Utils/Utils.swift
+++ InternalPoCWebViewTokenJWT/Utils/Utils.swift
@@ -31,12 +31,52 @@ class Utils{
     
     struct Constants {
         // Url's PRO -> http://sfcdwcs01/ss/Satellite?d=&pagename=AppNWLogin&idCliente=022692392V&canal=WMO
-        static let username = "022692392V" // Este idClient debe tener le formato CETELEM
+        static let username = "022692392V"
         static let password = "Ao14022022"
-        static let urlLab = "https://sfcdwcs01/ss/Satellite?d=&pagename=AppNWLogin&idCliente=70073260B&canal=WMO"
-        static let urlposicionglobal = "https://sfcdwcs01/posicion-global"
-        static let urlPRO = "https://www.dev-pass.carrefour.es/cs/Satellite?d=&pagename=AppNWLogin&idCliente=70073260B&canal=WMO"
-        static let tokenJWT = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IlJFUTBNVVE1TjBOQ1JUSkVNemszTTBVMVJrTkRRMFUwUTBNMVJFRkJSamhETWpkRU5VRkJRZyJ9.eyJpc3MiOiJodHRwczovL2ZpZG0uZ2lneWEuY29tL2p3dC8zXzhSX1lweGZocXFKRDRFeDFiZ24yOGxtTUV1eUQzd1MtYWc0Yl9xSnRMcFhJTVJPdHZtRjZIQXRPRzRXN1ZyNjUvIiwiYXBpS2V5IjoiM184Ul9ZcHhmaHFxSkQ0RXgxYmduMjhsbU1FdXlEM3dTLWFnNGJfcUp0THBYSU1ST3R2bUY2SEF0T0c0VzdWcjY1IiwiaWF0IjoxNjgwMTcwMzE0LCJleHAiOjE2ODAxNzA2MTQsInN1YiI6IjZmNjk0YmQwLWE5ZmMtNDAwNi05MzgzLTdmMTUyZTk5ZGEwNCIsInByb2ZpbGUudXNlcm5hbWUiOiIwOTE0MzcxNEgifQ.GGjWVv_8JcvVQfgTkeabYoH73blng0C3td-4AdLPgTdbEbs3q_dhBf45y04bUmUH9D-f44J0dqeFeMCzDCjf-hc3FnL-mHB6bZXLrTvjBdUwAdQCF-oCwrwQ0sHD2_TcivVIG2SzNRgGY6_XolGQ8w-k_KDkAiU9LYZ9VTn9axBaQixqKMEXmjGSlW8a95hvMgvACWupdPqGdWS_iGfhPYi-VbOkLfZP9P0BL2TScF6ZHvdLTxUq0fqMczJe6okbosk5vbY6cyCZGOO8VMCpcce5GCx38x9tOiqKcaS9nm-TnW1WMjJt_PShcddMnxoCYy4t6YR5jhnKVLW2-miP7A"
+        static let urlLab = "https://sfcdwcs01/ss/Satellite?d=&pagename=AppNWLogin&idCliente=09143714H&canal=APP"
+        static let urlposicionglobal = "https://www.pass.carrefour.es/zona-cliente/posicion-global/?beta=true"
+        static let urlDirect = "https://www.dev-pass.carrefour.es/solicitar-dinero"
+        static let urlPRO = "https://www.dev-pass.carrefour.es/cs/Satellite?d=&pagename=AppNWLogin&idCliente=09143714H&canal=APP"
+        static let tokenJWT = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IlJFUTBNVVE1TjBOQ1JUSkVNemszTTBVMVJrTkRRMFUwUTBNMVJFRkJSamhETWpkRU5VRkJRZyJ9.eyJpc3MiOiJodHRwczovL2ZpZG0uZ2lneWEuY29tL2p3dC8zX3o3alBycDZNb3BoSUdGTmtVT3pDcnh6OXNWU2hfbGZpSWwtS09TZENTN1A1bTBsMVNiQmNlZ0VFRjdxQWtzU1gvIiwiYXBpS2V5IjoiM196N2pQcnA2TW9waElHRk5rVU96Q3J4ejlzVlNoX2xmaUlsLUtPU2RDUzdQNW0wbDFTYkJjZWdFRUY3cUFrc1NYIiwiaWF0IjoxNjg0NzcxNjg1LCJleHAiOjE2ODQ3NzE5ODUsInN1YiI6IjZmNjk0YmQwLWE5ZmMtNDAwNi05MzgzLTdmMTUyZTk5ZGEwNCIsInByb2ZpbGUudXNlcm5hbWUiOiIwOTE0MzcxNEgifQ.YkLLtUvzFB7k9Gd_JYlaBh_N_M-Rx2ZwJL4p_98zv8M8pTeiuHmbVkSg_b0QBwtN5sRUBOnPLDTqiH2D70o6tob8357aMawkba8FSVhqn55cgxwCKN6_HChfiGHjpytiYUfo3EJbTyUI-G0bChk5gnOutViGN1MAdZ77DMhbkspwqUwBdNy71rbZWCMS6Bqbqavh9D5wwoLzxSUZTc4Fu0wyvDtSRHrUmdQzQWKD6Mh5h18VvinOrL28_5gfEaLgZv0st9rDMAHTVo4ogJgNxv7z29HvL3W4F9zhvlASHrSd00it2ZVKeDdXINAZTilRgPqjxbGkCTdg_NB9sQnDdg"
+    }
+    
+    func setupWebView(myWebViewContainer: UIView) -> WKWebView{
+        var auxiliar = WKWebView()
+        let userContentController = WKUserContentController()
+        
+        if let cookies = HTTPCookieStorage.shared.cookies {
+            let script = getJSCookiesString(for: cookies)
+            let cookieScript = WKUserScript(source: script, injectionTime: .atDocumentStart, forMainFrameOnly: false)
+            userContentController.addUserScript(cookieScript)
+        }
+        
+        let webViewConfig = WKWebViewConfiguration()
+        webViewConfig.allowsInlineMediaPlayback = true
+        webViewConfig.applicationNameForUserAgent = "Safari/605.1.15"
+        webViewConfig.userContentController = userContentController
+
+        auxiliar = WKWebView(frame: myWebViewContainer.bounds, configuration: webViewConfig)
+        return auxiliar
+    }
+    
+    ///Generates script to create given cookies
+    func getJSCookiesString(for cookies: [HTTPCookie]) -> String {
+        var result = ""
+        let dateFormatter = DateFormatter()
+        dateFormatter.timeZone = TimeZone(abbreviation: "UTC")
+        dateFormatter.dateFormat = "EEE, d MMM yyyy HH:mm:ss zzz"
+
+        for cookie in cookies {
+            result += "document.cookie='\(cookie.name)=\(cookie.value); domain=\(cookie.domain); path=\(cookie.path); "
+            if let date = cookie.expiresDate {
+                result += "expires=\(dateFormatter.string(from: date)); "
+            }
+            if (cookie.isSecure) {
+                result += "secure; "
+            }
+            result += "'; "
+        }
+        return result
     }
     
     
--- InternalPoCWebViewTokenJWT/Views/PosicionGLobalView/DirectViewController.swift
+++ InternalPoCWebViewTokenJWT/Views/PosicionGLobalView/DirectViewController.swift
@@ -12,7 +12,7 @@ class DirectViewController: UIViewController {
     
     // MARK: - Variables
     private var request : URLRequest {
-        let baseUrl = Utils.Constants.urlPRO
+        let baseUrl = Utils.Constants.urlposicionglobal
         let myURL = URL(string: baseUrl)!
         return URLRequest(url: myURL)
     }
@@ -29,35 +29,49 @@ class DirectViewController: UIViewController {
         self.myActivityIndicator.isHidden = true
         //print(Obfuscator().bytesByObfuscatingString(string: Utils.Constants.tokenJWT))
         // WebView + Delegate
+        
+        
         let webConfiguration = WKWebViewConfiguration()
             webConfiguration.allowsInlineMediaPlayback = true
             webConfiguration.applicationNameForUserAgent = "Safari/605.1.15"
         
         self.myWebView = WKWebView(frame: containerView.frame, configuration: webConfiguration)
-        self.containerView.addSubview(self.myWebView)
-        self.myWebView.translatesAutoresizingMaskIntoConstraints = false
         
-        //constrains for the webview to fit the containerView
-        let constraints = [
-            self.myWebView.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
-            self.myWebView.centerYAnchor.constraint(equalTo: containerView.centerYAnchor),
-            self.myWebView.widthAnchor.constraint(equalTo: containerView.widthAnchor),
-            self.myWebView.heightAnchor.constraint(equalTo: containerView.heightAnchor)
-        ]
+        let cookie = HTTPCookie(properties: [
+            .domain: "www.pass.carrefour.es",
+            .path: "/",
+            .name: "jwtToken",
+            .value: "\(Utils.Constants.tokenJWT)",
+            .secure: "TRUE",
+            .expires: NSDate(timeIntervalSinceNow: 31556926),
+        ])!
         
-        //activate the constrains and load the url
-        NSLayoutConstraint.activate(constraints)
-        myWebView.navigationDelegate = self
-        self.loadWebView(tokenJWT: Utils.Constants.tokenJWT)
+        self.myWebView.configuration.websiteDataStore.httpCookieStore.setCookie(cookie) {
+            self.myWebView.configuration.websiteDataStore.httpCookieStore.getAllCookies { cookies in
+
+                self.containerView.addSubview(self.myWebView)
+                self.myWebView.translatesAutoresizingMaskIntoConstraints = false
+                
+                //constrains for the webview to fit the containerView
+                let constraints = [
+                    self.myWebView.centerXAnchor.constraint(equalTo: self.containerView.centerXAnchor),
+                    self.myWebView.centerYAnchor.constraint(equalTo: self.containerView.centerYAnchor),
+                    self.myWebView.widthAnchor.constraint(equalTo: self.containerView.widthAnchor),
+                    self.myWebView.heightAnchor.constraint(equalTo: self.containerView.heightAnchor)
+                ]
+                
+                //activate the constrains and load the url
+                NSLayoutConstraint.activate(constraints)
+                self.myWebView.navigationDelegate = self
+                self.loadWebView()
+            }
+        }
     }
 
 
-    private func loadWebView(tokenJWT: String){
+    private func loadWebView(tokenJWT: String? = nil){
         if let theWebView = myWebView {
             var urlRequest = self.request
-            //urlRequest.allHTTPHeaderFields = ["Authorization": "Bearer \(tokenJWT)"]
-            //urlRequest.allHTTPHeaderFields = ["Content-Type": "application/json"]
-            //urlRequest.allHTTPHeaderFields = ["canal": "MIC4"]
             urlRequest.httpMethod = "GET"
             DispatchQueue.main.async {
                 theWebView.load(urlRequest)
@@ -65,21 +79,6 @@ class DirectViewController: UIViewController {
         }
     }
     
-    private func loadGlobalPosition() {
-        let baseUrl = Utils.Constants.urlposicionglobal
-        let myURL = URL(string: baseUrl)!
-        var myUrlRequest = URLRequest(url: myURL)
-        //urlRequest.allHTTPHeaderFields = ["Authorization": "Bearer \(tokenJWT)"]
-        //urlRequest.allHTTPHeaderFields = ["Content-Type": "application/json"]
-        //urlRequest.allHTTPHeaderFields = ["canal": "MIC4"]
-        myUrlRequest.httpMethod = "GET"
-        myUrlRequest.httpShouldHandleCookies = true
-        if let theWebView = myWebView {
-            DispatchQueue.main.async {
-                theWebView.load(myUrlRequest)
-            }
-        }
-    }
     
     private func showActivityIndicator(show: Bool) {
         if show{
@@ -97,12 +96,10 @@ class DirectViewController: UIViewController {
 extension DirectViewController: WKNavigationDelegate {
     
     func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
-        debugPrint("didStartProvisionalNavigation: ")
         self.showActivityIndicator(show: true)
     }
     
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        debugPrint("didFinish navigation: ")
         self.showActivityIndicator(show: false)
     }
     
@@ -110,20 +107,13 @@ extension DirectViewController: WKNavigationDelegate {
                  decidePolicyFor navigationAction: WKNavigationAction,
                  decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
         decisionHandler(.allow)
-        debugPrint("REQUEST NAVIGATION: "+(navigationAction.request.url?.absoluteString ?? ""))
     }
     
     func webView(_ webView: WKWebView,
                  decidePolicyFor navigationResponse: WKNavigationResponse,
                  decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
         decisionHandler(.allow)
-        debugPrint("RESPONSE: "+(navigationResponse.response.url?.absoluteString ?? ""))
-        
-        guard let NwLogin = navigationResponse.response.url?.absoluteString.contains("pagename=AppNWLogin") else { return }
-        
-        if NwLogin{
-            self.loadGlobalPosition()
-        }
+
             
     }
 
